d5d212e482bb5ae9fec52a46b85c872a06ab91d1
kristijan <jankuloski.k@gmail.com>
2023-09-13 20:17:09 +0200

    added firstname and lastname to register from

--- shipfinity.frontend/src/app/shared/models/register-form-data.ts
+++ shipfinity.frontend/src/app/shared/models/register-form-data.ts
@@ -1,5 +1,7 @@
 export default interface IRegisterFormModel {
     username: string;
+    firstName: string;
+    lastName: string;
     email: string;
     password: string;
     confirmPassword: string;
