851d50b5076c211c65821ab8498c82ecd2ae86bc
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2023-09-22 14:50:59 +0200

    Added Category: controller, services, dtos, mappers

---
+++ Shipfinity.Api/Controllers/CategoryController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Shipfinity.DTOs.CategoryDTOs;
+using Shipfinity.Services.Interfaces;
+using Shipfinity.Shared.Exceptions;
+
+namespace Shipfinity.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CategoryDto>> GetAllCategories()
+        {
+            try
+            {
+                var categories = await _categoryService.GetAllCategoriesAsync();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetCategoryById(int id)
+        {
+            if (id <= 0) return BadRequest("Invalid Id");
+
+            try
+            {
+                var category = await _categoryService.GetCategoryByIdAsync(id);
+                if (category == null)
+                {
+                    return NotFound($"Category with Id {id} not found");
+                }
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateCategory([FromBody] CreateCategoryDto createCategoryDto)
+        {
+            try
+            {
+                if (createCategoryDto == null || createCategoryDto.Name.IsNullOrEmpty())
+                {
+                    return BadRequest("Invalid input");
+                }
+
+                await _categoryService.CreateCategoryAsync(createCategoryDto);
+                return StatusCode(StatusCodes.Status201Created, "Category created");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCategory(int id, [FromBody] UpdateCategoryDto updateCategoryDto)
+        {
+            try
+            {
+                if (id != updateCategoryDto.Id)
+                {
+                    return BadRequest("Invalid Id");
+                };
+
+                await _categoryService.UpdateCategoryAsync(id, updateCategoryDto);
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCategoryById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Invalid Id");
+            }
+            try
+            {
+                await _categoryService.DeleteCategoryAsync(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
--- Shipfinity.Mappers/CategoryMappers.cs
+++ Shipfinity.Mappers/CategoryMappers.cs
@@ -1,4 +1,5 @@
 ﻿using Shipfinity.Domain.Models;
+using Shipfinity.DTOs.CategoryDTOs;
 using Shipfinity.ViewModels.CategoryViewModels;
 using System;
 using System.Collections.Generic;
@@ -10,12 +11,30 @@ namespace Shipfinity.Mappers
 {
     public static class CategoryMappers
     {
-        public static Category ToCategory(this CategoryCreateViewModel cat)
+
+        public static CategoryDto MapToDto(Category category)
+        {
+            return new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+            };
+        }
+
+        public static Category MapToCategory(CreateCategoryDto createCategoryDto)
         {
             return new Category
             {
-                Name = cat.Name,
-                Products = new List<Product>()
+                Name = createCategoryDto.Name,
+            };
+        }
+
+        public static Category MapToCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            return new Category
+            {
+                Id = updateCategoryDto.Id,
+                Name = updateCategoryDto.Name,
             };
         }
     }
