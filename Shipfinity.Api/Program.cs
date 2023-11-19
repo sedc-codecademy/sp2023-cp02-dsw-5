d3ecfc1ff2e2292a6853d2741c86fbf4a7f60a25
Kristijan <jankuloski.k@gmail.com>
2023-11-19 12:26:14 +0100

    Removed comment

--- Shipfinity.Api/Program.cs
+++ Shipfinity.Api/Program.cs
@@ -5,8 +5,6 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
