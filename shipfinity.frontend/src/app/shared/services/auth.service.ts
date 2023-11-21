48378cc5f9026415251d94d354b09c624b9b5c1f
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2023-11-21 14:52:27 +0100

    saveLocalUser

--- shipfinity.frontend/src/app/shared/services/auth.service.ts
+++ shipfinity.frontend/src/app/shared/services/auth.service.ts
@@ -5,37 +5,48 @@ import { environment } from 'src/environments/environment';
 import IRegisterFormModel from '../models/register-form-data';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   get isLoggedIn() {
-    return !!localStorage.getItem("Token");
+    return !!localStorage.getItem('Token');
   }
 
-  public setToken(token: string){
-    localStorage.setItem("Token", token);
+  public setToken(token: string) {
+    localStorage.setItem('Token', token);
   }
 
-  public getToken(){
-    localStorage.getItem("Token");
+  public getToken() {
+    localStorage.getItem('Token');
   }
 
-  public deleteToken(){
-    localStorage.removeItem("Token");
+  public deleteToken() {
+    localStorage.removeItem('Token');
   }
 
   public login(request: ILoginFormData) {
     return this.http.post(`${environment.API_URL}/auth/login`, request);
   }
 
-  public register(requestData: IRegisterFormModel){
+  public register(requestData: IRegisterFormModel) {
     return this.http.post(`${environment.API_URL}/auth/register`, requestData);
   }
 
-  public checkUsername(username: string){
-    return this.http.get(`${environment.API_URL}/auth/check?username=${username}`);
+  public checkUsername(username: string) {
+    return this.http.get(
+      `${environment.API_URL}/auth/check?username=${username}`
+    );
+  }
+
+  public saveUserInLocalStorage(user: IRegisterFormModel) {
+    localStorage.setItem('User', JSON.stringify(user));
+  }
+
+  public getUserFromLocalStorage(): IRegisterFormModel | null {
+    const stringUserData = localStorage.getItem('User');
+
+    return stringUserData ? JSON.parse(stringUserData) : null;
   }
 }
