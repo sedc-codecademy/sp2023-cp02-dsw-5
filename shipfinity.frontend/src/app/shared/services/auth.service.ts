d3ca6f21722cbc73e7e4e77687aa6bbdf8ae3e05
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2023-11-23 13:50:40 +0100

    added AuthApiService and User model

--- shipfinity.frontend/src/app/shared/services/auth.service.ts
+++ shipfinity.frontend/src/app/shared/services/auth.service.ts
@@ -1,13 +1,26 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import ILoginFormData from '../models/login-form-data';
 import { environment } from 'src/environments/environment';
 import IRegisterFormModel from '../models/register-form-data';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../models/user';
+import { AuthApiService } from './api/auth-api.service';
+import { Router } from '@angular/router';
+import { NotificationService } from './notification.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private authApiService = inject(AuthApiService);
+  private router = inject(Router);
+  private notificationService = inject(NotificationService);
+
+  public currentUser$ = new BehaviorSubject<User>(
+    this.getUserFromLocalStorage()
+  );
+
   constructor(private http: HttpClient) {}
 
   get isLoggedIn() {
@@ -26,8 +39,33 @@ export class AuthService {
     localStorage.removeItem('Token');
   }
 
+  /*
   public login(request: ILoginFormData) {
     return this.http.post(`${environment.API_URL}/auth/login`, request);
+  }
+  */
+
+  public login(username: string, password: string) {
+    this.authApiService.loginUser(username, password).subscribe({
+      next: (res) => {
+        console.log(res);
+
+        const token = res.headers.get('access-token');
+        console.log(token);
+
+        const user = res.body as User;
+        console.log(user);
+
+        this.saveUserInLocalStorage(user);
+        this.currentUser$.next(user);
+        this.router.navigate(['home']);
+        this.notificationService.successMessage('Successfully logged in!');
+      },
+      error: (error) => {
+        console.log(error);
+        this.notificationService.errorMessage(error.error.message);
+      },
+    });
   }
 
   public register(requestData: IRegisterFormModel) {
@@ -40,12 +78,12 @@ export class AuthService {
     );
   }
 
-  public saveUserInLocalStorage(user: IRegisterFormModel) {
-    localStorage.setItem('User', JSON.stringify(user));
+  saveUserInLocalStorage(user: User) {
+    localStorage.setItem('currentUser', JSON.stringify(user));
   }
 
-  public getUserFromLocalStorage(): IRegisterFormModel | null {
-    const stringUserData = localStorage.getItem('User');
+  getUserFromLocalStorage(): User {
+    const stringUserData = localStorage.getItem('currentUser');
 
     return stringUserData ? JSON.parse(stringUserData) : null;
   }
