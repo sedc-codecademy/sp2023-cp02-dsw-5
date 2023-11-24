80d0e6661f572816b26699a4d8edca24b1752b4b
cheppymk <toso.micevski@gmail.com>
2023-11-24 16:51:50 +0100

    New table Address New migration Pls update the database ASAP

---
+++ Shipfinity.Domain/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using Shipfinity.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipfinity.Domain.Models
+{
+    public class Address : BaseEntity
+    {
+        public string AddressLine { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Order> Orders { get; set; } = new();
+        public List<Customer> Customers { get; set; } = new();
+    }
+}
