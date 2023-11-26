aa03c4c831fe3df32a8cf8b04bd9a0b9bbcd4639
Aleksandar Todoroski <todoroski.aleksandar@gmail.com>
2023-11-26 22:44:54 +0100

    review product endpoint created

--- Shipfinity.Services/Interfaces/IProductService.cs
+++ Shipfinity.Services/Interfaces/IProductService.cs
@@ -14,5 +14,6 @@ namespace Shipfinity.Services.Interfaces
         Task<List<ProductReadDto>> GetProductsOnSaleAsync();
         Task<List<ProductReadDto>> GetProductsInRangeAsync(int skip, int take);
         Task UpdateProductPhotoUrl(int productId, string photoUrl);
+        Task<ReviewProduct> CreateReviewProductAsync(int productId, ReviewProductDto reviewProductDto);
     }
 }
