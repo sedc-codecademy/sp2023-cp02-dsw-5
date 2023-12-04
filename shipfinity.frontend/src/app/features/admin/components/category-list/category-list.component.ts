2db7a82a780408d4c82a9785e836376379e76108
kristijan <jankuloski.k@gmail.com>
2023-12-04 19:03:25 +0100

    Created category list

--- shipfinity.frontend/src/app/features/admin/components/category-list/category-list.component.ts
+++ shipfinity.frontend/src/app/features/admin/components/category-list/category-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryModel } from 'src/app/shared/models/category';
+import { CategoryService } from 'src/app/shared/services/category.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
+  categories$ = this.categoryService.categoryList$;
 
+  constructor(private categoryService: CategoryService){}
+
+  ngOnInit(): void {
+    this.categoryService.getCategories();
+  }
 }
