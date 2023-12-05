d251b8248872a21c57735d4afeebacdf561fb39b
Aleksandar1105 <todoroski.aleksandar@gmail.com>
2023-12-05 14:19:40 +0100

    product-card-details created

--- shipfinity.frontend/src/app/features/product/components/product/product.component.ts
+++ shipfinity.frontend/src/app/features/product/components/product/product.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './../../../../shared/services/shopping-cart.service';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import Product from 'src/app/shared/models/product';
+import ProductOrder from 'src/app/shared/models/product-order';
+import { NotificationService } from 'src/app/shared/services/notification.service';
 import { ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
@@ -8,16 +12,25 @@ import { ProductService } from 'src/app/shared/services/product.service';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  @Input() product: Product = new Product();
   productDetails$ = this.productService.productDetails$;
 
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private shoppingCartService: ShoppingCartService,
+    private notificationService: NotificationService
   ) {}
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
     const idFromRoute = Number(routeParams.get('id'));
 
     this.productService.GetByProductId(idFromRoute);
+  }
+
+  addToCartClick() {
+    const product = this.productDetails$.value;
+    this.shoppingCartService.addItem(new ProductOrder(product, 1));
+    this.notificationService.successMessage(`${product.name} added to cart`);
   }
 }
