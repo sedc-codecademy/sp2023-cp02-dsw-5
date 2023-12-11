cc45372fbcd1fc5bfb7bff5fb5dace2148c1ffb1
cheppymk <toso.micevski@gmail.com>
2023-12-11 01:37:44 +0100

    Revert "Revert "sign up page with api seller register""
    This reverts commit a1957641d62a53a0260c358031a6f81b6de868da.

--- shipfinity.frontend/src/app/register/register.component.ts
+++ shipfinity.frontend/src/app/register/register.component.ts
@@ -7,51 +7,60 @@ import IRegisterFormModel from '../shared/models/register-form-data';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-    username: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
-    firstName: new FormControl('', [Validators.required, Validators.maxLength(30)]),
-    lastName: new FormControl('', [Validators.required, Validators.maxLength(30)]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-    passwordRepeated: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email, Validators.maxLength(30)])
-  }, { validators: passwordConfirmValidator() });
+  registerForm = new FormGroup(
+    {
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(30),
+      ]),
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(30),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      passwordRepeated: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(30),
+      ]),
+    },
+    { validators: passwordConfirmValidator() }
+  );
   usernameTaken: boolean = false;
 
-  constructor(private auth: AuthService){}
+  constructor(private auth: AuthService) {}
 
   onRegisterSubmit() {
-    if(!this.registerForm.valid){
+    if (!this.registerForm.valid) {
       return;
     }
-    this.auth.checkUsername(this.registerForm.value.username!).subscribe({next: this.usernameIsValid, error: this.usernameInvalid});
+    this.auth
+      .checkUsername(this.registerForm.value.username!)
+      .subscribe({ next: this.usernameInvalid, error: this.usernameInvalid });
   }
-
-  usernameIsValid = (data: any) => {
-    this.usernameTaken = false;
-    const formData : IRegisterFormModel = {
-      username: this.registerForm.value.username!,
-      firstName: this.registerForm.value.firstName!,
-      lastName: this.registerForm.value.lastName!,
-      email: this.registerForm.value.email!,
-      password: this.registerForm.value.password!,
-      confirmPassword: this.registerForm.value.passwordRepeated!
-    }
-    this.auth.register(formData).subscribe({next: this.registerSuccessful, error: this.registerFailed})
-  }
-
   usernameInvalid = (error: any) => {
     this.usernameTaken = true;
     console.error(error);
-  }
+  };
 
   registerSuccessful = (data: any) => {
     // Notify they've been registered or login
-  }
+    console.log('Registration successful', data);
+  };
 
   registerFailed = (error: any) => {
     console.error(error);
-  }
+  };
 }
---
+++ shipfinity.frontend/src/app/shared/models/register-seller-form-data.ts
@@ -0,0 +1,9 @@
+export interface IRegisterSeller {
+  username: string;
+  password: string;
+  confirmPassword: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  name: string;
+}
