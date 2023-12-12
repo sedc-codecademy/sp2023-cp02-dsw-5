d07a9c2a9e1c577e8a8e06ba23bb011d4d2cc64f
cheppymk <toso.micevski@gmail.com>
2023-12-12 14:18:38 +0100

    search functionality for products

--- shipfinity.frontend/src/app/features/product/components/search/search.component.ts
+++ shipfinity.frontend/src/app/features/product/components/search/search.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SearchService } from 'src/app/shared/services/search.service';
 import Product from '../../../../shared/models/product';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
-  keyword: string = "";
+export class SearchComponent implements OnInit {
+  keyword: string = '';
   results: Product[] = [];
 
-  constructor(private route: ActivatedRoute){}
+  constructor(
+    private route: ActivatedRoute,
+    private searchService: SearchService
+  ) {}
 
-  ngOnInit(){
-    this.route.queryParams
-      .subscribe(params => {
-        this.keyword = params['search'];
-      });
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.keyword = params['search'];
+      this.searchProducts();
+    });
+  }
+
+  private searchProducts() {
+    this.searchService.searchProducts(this.keyword).subscribe(
+      (data) => (this.results = data),
+      (error) => console.error(error)
+    );
   }
 }
--- shipfinity.frontend/src/app/features/product/product.routes.ts
+++ shipfinity.frontend/src/app/features/product/product.routes.ts
@@ -1,14 +1,17 @@
-import { Routes } from "@angular/router";
-import { CategoryComponent } from "./components/category/category.component";
-import { SearchComponent } from "./components/search/search.component";
-import { ProductComponent } from "./components/product/product.component";
-import { SaleComponent } from "./components/sale/sale.component";
+import { Routes } from '@angular/router';
+import { CategoryComponent } from './components/category/category.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductComponent } from './components/product/product.component';
+import { SaleComponent } from './components/sale/sale.component';
 
 export const productRoutes: Routes = [
-    { path: '', children: [
-        { path: 'category/:id', component: CategoryComponent },
-        { path: 'search', component: SearchComponent },
-        { path: 'sale', component: SaleComponent },
-        { path: ':id', component: ProductComponent }
-    ] }
-]
+  {
+    path: '',
+    children: [
+      { path: 'category/:id', component: CategoryComponent },
+      { path: 'search', component: SearchComponent },
+      { path: 'sale', component: SaleComponent },
+      { path: ':id', component: ProductComponent },
+    ],
+  },
+];
